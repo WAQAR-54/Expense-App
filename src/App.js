de9119594b74c813eb2bc3c6afebb870dfd58e3f
WAQAR-54 <56077768+WAQAR-54@users.noreply.github.com>
2020-07-13 23:52:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Header } from './Components/TransactionHistory';
+import Header from './Components/Header';
+import Balance from './Components/Balance';
+import AccountSummery from './Components/AccountSummery';
+import TransactionHistory from  './Components/TransactionHistory';
+import AddTransaction from './Components/AddTransaction';
 
 function App() {
   return (
     <div >
-   Hello
-     
+            <Header/>
+           <div className="container">
+             <Balance></Balance>
+             <AccountSummery></AccountSummery>
+             <TransactionHistory></TransactionHistory>
+             <AddTransaction></AddTransaction>
+           </div>
+          
     </div>
   );
 }
