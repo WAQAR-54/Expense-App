ef5c9898ade8e0093a5ef45b1f0dddb7a757d9b6
WAQAR-54 <56077768+WAQAR-54@users.noreply.github.com>
2020-07-13 23:51:52 +0500

    Update Balance.js

---
+++ src/Components/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Balance() {
+  return (
+    <div>
+        <h3> Current Balance  </h3>
+        <h1>$0.00</h1>
+    </div>
+  );
+}
+
+export default Balance;
