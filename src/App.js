e3a5cb1b98b94fec109c0aa9802adebadbea8a48
WAQAR-54 <56077768+WAQAR-54@users.noreply.github.com>
2020-07-15 22:53:36 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import './App.css';
-import Header from './Components/Header';
-import Balance from './Components/Balance';
-import AccountSummery from './Components/AccountSummery';
-import TransactionHistory from  './Components/TransactionHistory';
-import AddTransaction from './Components/AddTransaction';
 
-//import provider
-import {GlobalProvider} from './context/GlobalState'
+// Import CSS
+import './App.css';
+
+
+
 function App() {
   return (
     <div className="bgcontainer">
-      <GlobalProvider >
-       <Header />
-        <div className="container">
-          <Balance />
-          <AccountSummery />
-          <TransactionHistory />
-          <AddTransaction />
-        </div>
-      </GlobalProvider>
+     hello
     </div>
   );
 }
