c6ced9e1cc9404ef5c45c42a92d4c96dd12bd664
WAQAR-54 <56077768+WAQAR-54@users.noreply.github.com>
2020-07-17 19:06:31 +0500

    Finl

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import React from 'react';
 // Import CSS
 import './App.css';
 
+// Import Components
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { AccountSummary } from './components/AccountSummary';
+import { TransactionHistory } from './components/TransactionHistory';
+import { AddTransaction } from './components/AddTransaction';
 
+// Import Provider
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
     <div className="bgcontainer">
-     hello
+      <GlobalProvider >
+        <Header />
+        <div className="container">
+          <Balance />
+          <AccountSummary />
+          <TransactionHistory />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
     </div>
   );
 }
